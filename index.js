774630409c3978ab711b9c466287d5b6e7cef83d
yogeshkumarpatil <appseeds.yogeshkumar@gmail.com>
2023-05-19 21:24:31 +0530

    video no. is complete

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var x = 20;
+//use fs module at the top level because it is recomonded but still work on low level.
+const fs = require('fs');
+console.log("Yogesh Patil");
 
-const arr = [2,4,6,8,1,5,,8];
-console.log(arr[0]);
+// navin file tayar karnasathi khalil commond use kartat.he non global module aahe mahanun yala imort karave lagte.
+// fs.writeFileSync("hello22.txt", "another file")
+
+//show the directory name => use this in big project
+console.log("=>",__filename);
+
