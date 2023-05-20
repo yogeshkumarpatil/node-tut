e4d486caab0d6a582228acf4ce4f7a5ef2171767
yogeshkumarpatil <appseeds.yogeshkumar@gmail.com>
2023-05-20 21:07:30 +0530

    Video No. 10 is complete

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const http = require('http');
-
-
-
-http.createServer((req, resp) =>
-{
-    resp.write("<h1>Hello this is Yogesh Patil hhhhh  jjjj gg<h1>");
-    resp.end();
-}).listen(4500);
+console.warn("Yogesh Patil  ");
+console.log(100+20);
